433e5490e885b3aadd6e03251d039835f142c7c1
Vikas B N <vikas@eternaldawn.in>
2012-05-01 00:00:07 +0530

    Property for disabling boot animation is different in cm6, so reverting to that
    Change-Id: Ic88f1e1964996be4da5c9c807fea2e56ea549be3

--- src/com/cyanogenmod/cmparts/activities/PerformanceSettingsActivity.java
+++ src/com/cyanogenmod/cmparts/activities/PerformanceSettingsActivity.java
@@ -52,7 +52,7 @@ public class PerformanceSettingsActivity extends PreferenceActivity implements P
 
     private static final String DISABLE_BOOTANIMATION_PREF = "pref_disable_bootanimation";
 
-    private static final String DISABLE_BOOTANIMATION_PERSIST_PROP = "persist.sys.nobootanimation";
+    private static final String DISABLE_BOOTANIMATION_PERSIST_PROP = "debug.sf.nobootanimation";
 
     private static final String DISABLE_BOOTANIMATION_DEFAULT = "0";
 
